07f685a5245181c1bf62a40a6e00426a5ea95c32
ashhadahsan <63206988+ashhadahsan@users.noreply.github.com>
2024-07-20 14:10:29 +0200

    Party Added

--- src/java/uko/evm/models/Party.java
+++ src/java/uko/evm/models/Party.java
@@ -9,5 +9,53 @@ package uko.evm.models;
  * @author ashhad
  */
 public class Party {
+    private int id;
+    private int customOrder;
+    private String name;
+    private String alias;
+    private boolean isPartylist;
+
+    private int votes;
     
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCustomOrder() {
+        return customOrder;
+    }
+    public void setCustomOrder(int customOrder) {
+        this.customOrder = customOrder;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    public boolean isPartylist() {
+        return isPartylist;
+    }
+    public void setPartylist(boolean isPartylist) {
+        this.isPartylist = isPartylist;
+    }
+
+    public int getVotes() {
+        return votes;
+    }
+    public void setVotes(int votes) {
+        this.votes = votes;
+    }
 }
